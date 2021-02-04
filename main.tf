3500d889f40dcbd4986064ffca4ec0d2302e70fd
Pavel Pichrt <pavel.pichrt@reachplc.com>
2021-02-04 14:15:22 +0000

    fixes

--- main.tf
+++ main.tf
@@ -56,72 +56,111 @@ resource "aws_iam_role" "firehose_role" {
 }
 
 resource "aws_iam_policy" "firehose_policy" {
-  name   = "firehose-${var.stream_name}-policy"
-  policy = jsonencode({
-    Version = "2012-10-17"
-
-    Statement = {
-      Sid = ""
-      Effect = "Allow"
-      Action = ["glue:GetTable", "glue:GetTableVersion", "glue:GetTableVersions"]
-      Resource = ["arn:aws:glue:${var.region}:${var.account_id}:catalog", "arn:aws:glue:${var.region}:${var.account_id}:database/%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%", "arn:aws:glue:${var.region}:${var.account_id}:table/%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%/%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%"]
-    }
-
-    Statement = {
-      Sid = ""
-      Effect = "Allow"
-      Action = ["s3:AbortMultipartUpload", "s3:GetBucketLocation", "s3:GetObject", "s3:ListBucket", "s3:ListBucketMultipartUploads", "s3:PutObject"]
-      Resource = ["${var.target_s3_bucket_arn}", "${var.target_s3_bucket_arn}/*"]
-    }
-
-    Statement = {
-      Sid = ""
-      Effect = "Allow"
-      Action = ["lambda:InvokeFunction", "lambda:GetFunctionConfiguration"]
-      Resource = "${var.transform_lambda_arn}:*"
-    }
-
-    Statement = {
-      Effect = "Allow"
-      Action = ["kms:GenerateDataKey", "kms:Decrypt"]
-      Resource = ["arn:aws:kms:${var.region}:${var.account_id}:key/%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%"]
-      Condition StringEquals {
-        "kms:ViaService" = "s3.${var.region}.amazonaws.com"
-      }
-
-      Condition StringLike {
-        "kms:EncryptionContext:aws:s3:arn" = ["arn:aws:s3:::%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%/*"]
-      }
-    }
-
-    "Statement" = {
-      Sid = ""
-      Effect = "Allow"
-      Action = ["logs:PutLogEvents"]
-      Resource = ["${aws_cloudwatch_log_group.firehose_log_group.arn}:*"]
-    }
-
-    Statement = {
-      Sid = ""
-      Effect = "Allow"
-      Action = ["kinesis:DescribeStream", "kinesis:GetShardIterator", "kinesis:GetRecords", "kinesis:ListShards"]
-      Resource = "arn:aws:kinesis:${var.region}:${var.account_id}:stream/%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%"
-    }
-
-    Statement = {
-      Effect = "Allow"
-      Action = ["kms:Decrypt"]
-      Resource = ["arn:aws:kms:${var.region}:${var.account_id}:key/%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%"]
-
-      Condition StringEquals {
-        "kms:ViaService" = "kinesis.${var.region}.amazonaws.com"
-      }
-
-      Condition StringLike {
-        "kms:EncryptionContext:aws:kinesis:arn" = "arn:aws:kinesis:${var.region}:${var.account_id}:stream/%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%"
-      }
-    }
-  })
+  name   = "firehose-${var.stream_name}"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "",
+            "Effect": "Allow",
+            "Action": [
+                "glue:GetTable",
+                "glue:GetTableVersion",
+                "glue:GetTableVersions"
+            ],
+            "Resource": [
+                "arn:aws:glue:${var.region}:${var.account_id}:catalog",
+                "arn:aws:glue:${var.region}:${var.account_id}:database/%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%",
+                "arn:aws:glue:${var.region}:${var.account_id}:table/%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%/%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%"
+            ]
+        },
+        {
+            "Sid": "",
+            "Effect": "Allow",
+            "Action": [
+                "s3:AbortMultipartUpload",
+                "s3:GetBucketLocation",
+                "s3:GetObject",
+                "s3:ListBucket",
+                "s3:ListBucketMultipartUploads",
+                "s3:PutObject"
+            ],
+            "Resource": [
+                "${var.target_s3_bucket_arn}",
+                "${var.target_s3_bucket_arn}/*"
+            ]
+        },
+        {
+            "Sid": "",
+            "Effect": "Allow",
+            "Action": [
+                "lambda:InvokeFunction",
+                "lambda:GetFunctionConfiguration"
+            ],
+            "Resource": "${var.transform_lambda_arn}:*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "kms:GenerateDataKey",
+                "kms:Decrypt"
+            ],
+            "Resource": [
+                "arn:aws:kms:${var.region}:${var.account_id}:key/%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%"
+            ],
+            "Condition": {
+                "StringEquals": {
+                    "kms:ViaService": "s3.${var.region}.amazonaws.com"
+                },
+                "StringLike": {
+                    "kms:EncryptionContext:aws:s3:arn": [
+                        "arn:aws:s3:::%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%/*"
+                    ]
+                }
+            }
+        },
+        {
+            "Sid": "",
+            "Effect": "Allow",
+            "Action": [
+                "logs:PutLogEvents"
+            ],
+            "Resource": [
+                "${aws_cloudwatch_log_group.firehose_log_group.arn}:*"
+            ]
+        },
+        {
+            "Sid": "",
+            "Effect": "Allow",
+            "Action": [
+                "kinesis:DescribeStream",
+                "kinesis:GetShardIterator",
+                "kinesis:GetRecords",
+                "kinesis:ListShards"
+            ],
+            "Resource": "arn:aws:kinesis:${var.region}:${var.account_id}:stream/%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "kms:Decrypt"
+            ],
+            "Resource": [
+                "arn:aws:kms:${var.region}:${var.account_id}:key/%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%"
+            ],
+            "Condition": {
+                "StringEquals": {
+                    "kms:ViaService": "kinesis.${var.region}.amazonaws.com"
+                },
+                "StringLike": {
+                    "kms:EncryptionContext:aws:kinesis:arn": "arn:aws:kinesis:${var.region}:${var.account_id}:stream/%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%"
+                }
+            }
+        }
+    ]
+}
+EOF
 }
 
 resource "aws_iam_role_policy_attachment" "apps_lambda_role_policy_attachment" {
